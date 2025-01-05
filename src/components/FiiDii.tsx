aefad4cddc8ae5ae4c29e09c29d8ca12b315ad05
Kavish Ambani <kavishkkambani@gmail.com>
2025-01-06 02:10:57 +0530

    bug fix

--- src/components/FiiDii.tsx
+++ src/components/FiiDii.tsx
@@ -6,7 +6,7 @@ export const FiiDii = () => {
     const [diiData] = useState(-244.75); // Negative value for DII
     return (
         <div className=" rounded-md bg-[#fff] dark:bg-[#1c1d1f]">
-            <FIIDIIActivityChart fiiData={fiiData} diiData={diiData} isDarkMode={false}/>
+            <FIIDIIActivityChart fiiData={fiiData} diiData={diiData}/>
         </div>
     )
 }
