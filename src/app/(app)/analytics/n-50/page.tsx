b0fdd8ee64d05879d31c59bbd1789e0145c1d4ca
Kavish Ambani <kavishkkambani@gmail.com>
2025-06-06 13:53:11 +0530

    fix css

--- src/app/(app)/analytics/n-50/page.tsx
+++ src/app/(app)/analytics/n-50/page.tsx
@@ -37,7 +37,7 @@ function Page() {
     return (
         <>
             <Navbar />
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 min-h-screen">
                 <h1 className="text-2xl sm:text-3xl font-semibold text-center mb-6">
                     Nifty 50 Stocks
                 </h1>
