5380675112c4b0944a5b754a760ec9cc1083ab34
Kavish Ambani <kavishkkambani@gmail.com>
2025-06-19 01:45:16 +0530

    refactor: replace div with main for semantic HTML structure in Home component

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <Navbar/>
-        <div className="flex-grow flex justify-center min-h-screen">
+        <main className="flex-grow flex justify-center min-h-screen">
           <h1 className="text-3xl p-16">Empower your Financial Future</h1>
-        </div>
+        </main>
       <Footer/>
     </div>
   );
